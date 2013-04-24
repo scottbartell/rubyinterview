5d48ee99d44e0c50fd69996395915393c51c36a7
Scott Bartell <me@scottbartell.com>
2013-04-24 03:56:06 -0400

    fix typo

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ working_directory "/home/deployer/apps/rinterview_production/current"
 stderr_path "/home/deployer/apps/rinterview_production/current/log/unicorn.log"
 pid "/home/deployer/apps/rinterview_production/current/tmp/pids/unicorn.pid"
 
-preload_app: true
+preload_app true
 listen "/tmp/unicorn_rinterview_production.sock"
 worker_processes 1
